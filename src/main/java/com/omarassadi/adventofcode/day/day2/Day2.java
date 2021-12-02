ebb01d0606c49a2bc30f189decd36c11ce593049
Omar Assadi <omar@assadi.co.il>
2021-12-02 08:07:40 -0500

    Remove unused utilities

--- src/main/java/com/omarassadi/adventofcode/day/day2/Day2.java
+++ src/main/java/com/omarassadi/adventofcode/day/day2/Day2.java
@@ -2,22 +2,16 @@ package com.omarassadi.adventofcode.day.day2;
 
 import com.omarassadi.adventofcode.day.Day;
 import com.omarassadi.adventofcode.day.PuzzleSolution;
-import com.omarassadi.adventofcode.util.StreamExUtil;
-import com.omarassadi.adventofcode.util.TupleUtil;
 import com.omarassadi.adventofcode.util.io.input.parser.InputParser;
 import com.omarassadi.adventofcode.util.math.Vector2;
 import com.omarassadi.adventofcode.util.math.Vector3;
-import lombok.experimental.ExtensionMethod;
 import one.util.streamex.StreamEx;
-import org.javatuples.Pair;
-import org.javatuples.Triplet;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Stream;
 
 @Component
-@ExtensionMethod({StreamEx.class, StreamExUtil.class, Pair.class, Triplet.class, TupleUtil.class})
 public class Day2 extends Day<Stream<Command>, Long> {
 
     public Day2(final InputParser<Stream<Command>> inputParser) {
