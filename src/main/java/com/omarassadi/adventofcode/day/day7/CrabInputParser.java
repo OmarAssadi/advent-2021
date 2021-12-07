930d05a963d799dcf8a9261a8788d11f74ed69db
Omar Assadi <omar@assadi.co.il>
2021-12-07 01:08:26 -0500

    Remove accidental c&p from day 6

--- src/main/java/com/omarassadi/adventofcode/day/day7/CrabInputParser.java
+++ src/main/java/com/omarassadi/adventofcode/day/day7/CrabInputParser.java
@@ -5,27 +5,15 @@ import one.util.streamex.StreamEx;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Stream;
 
-import static com.omarassadi.adventofcode.day.day6.Day6.STARTING_DAYS;
 import static java.util.function.Predicate.not;
 @Component
 public class CrabInputParser implements InputParser<List<Long>> {
     @Override
     public List<Long> parse(final Stream<String> input) {
         return StreamEx.of(input).flatMap(line -> Arrays.stream(line.split(","))
-                .filter(not(String::isBlank)).map(Long::parseLong)).toList();/*foldLeft(new HashMap<Long, Long>(), (acc, value) -> {
-                    acc.merge(value, 1L, Long::sum);
-                    return acc;
-        });*/
-        /*return StreamEx.of(input).flatMap(line -> Arrays.stream(line.split(","))
-                .filter(not(String::isBlank)).map(Integer::parseInt))
-            .foldLeft(new long[STARTING_DAYS + 1], (freq, timer) -> {
-                freq[timer]++;
-                return freq;
-            });*/
+                .filter(not(String::isBlank)).map(Long::parseLong)).toList();
     }
 }
